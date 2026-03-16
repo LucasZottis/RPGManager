a110be46503f0dcb9b4bd32974f98c6f622c8f33
Lucas Zottis <zottis.lucas@hotmail.com>
2026-03-16 19:38:26 -0300

    Criação de projeto angular

---
+++ RPGManager.Desktop/RPGManager.Desktop.Migration.PostegreSql/ContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using RPGManager.Desktop.Infra.Db.Contexts;
+using RPGManager.Desktop.Infra.Db.DbContextConfigurator;
+using RPGManager.Desktop.Infra.Db.Enums;
+using RPGManager.Desktop.Infra.Options;
+
+namespace RPGManager.Desktop.SqLite;
+
+public class ContextFactory : IDesignTimeDbContextFactory<Context>
+{
+    public Context CreateDbContext( string[] args )
+    {
+        var options = Options.Create( new DataBaseSettings
+        {
+            DataBaseType = DataBaseType.SqLite,
+            ConnectionStrings = new ConnectionStrings
+            {
+                SqLite = $"Data Source={AppContext.BaseDirectory}\\DB\\rpg_manager.db"
+            }
+        } );
+
+        return new Context(
+            new DbContextConfigurator( options )
+        );
+    }
+}
